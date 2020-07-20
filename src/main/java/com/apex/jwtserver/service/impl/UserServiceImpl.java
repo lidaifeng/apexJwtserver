b4449e61218d0c714b63185f825128bca41af3b8
中银-李代锋 <daifeng.li@bocichina.com>
2020-07-20 09:09:32 +0800

    加上selectByUserName方法

--- src/main/java/com/apex/jwtserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/apex/jwtserver/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.apex.jwtserver.service.impl;
 
 
+import com.apex.jwtserver.entity.User;
 import com.apex.jwtserver.mapper.UserMapper;
 import com.apex.jwtserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +12,8 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Override
+    public User selectByUserName(String userName) {
+        return null;
+    }
 }
